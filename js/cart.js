5c17c78548e8611e9c2e5f5de8a59bb480d76031
Tomas Maneri <tomasabelmaneri@gmail.com>
2024-06-04 15:16:39 -0300

    cart js very difficult

--- js/cart.js
+++ js/cart.js
@@ -13,6 +13,60 @@ if (productsInCart) {
     ContainerCartActions.classList.remove("disabled");
     ContainerPurchasedCart.classList.remove("disabled");
 
+    ContainerCartProducts.innerHTML = "";
+
+    productsInCart.forEach(product => {
+
+        const div = document.createElement("div");
+        div.classList.add("product-cart");
+        div.innerHTML = `
+        <img class="image-product-cart" src="${product.image}" alt="${product.title}">
+        <div class="name-product-cart">
+            <small>Title</small>
+            <h3> Rare Sneaker 01</h3>
+        </div>
+        <div class="quantity-product-cart">
+            <small>Quantity</small>
+            <p>1</p>
+        </div>
+        <div class="price-product-cart">
+            <small>Price</small>
+            <p>$400</p>
+        </div>
+        <div class="subtotal-product-cart">
+            <small>Subtotal</small>
+            <p>$400</p>
+        </div>
+        <button class="delete-product-cart"><i class="bi bi-trash3-fill"></i></button>
+        `;
+
+        ContainerCartProducts.append(div);
+    })
+
+
 } else {
 
 }
+
+
+
+{/* <div class="product-cart">
+            <img class="image-product-cart" src="../img/rare-sneakers-01.jpg" alt="">
+            <div class="name-product-cart">
+                <small>Title</small>
+                <h3> Rare Sneaker 01</h3>
+            </div>
+            <div class="quantity-product-cart">
+                <small>Quantity</small>
+                <p>1</p>
+            </div>
+            <div class="price-product-cart">
+                <small>Price</small>
+                <p>$400</p>
+            </div>
+            <div class="subtotal-product-cart">
+                <small>Subtotal</small>
+                <p>$400</p>
+            </div>
+            <button class="delete-product-cart"><i class="bi bi-trash3-fill"></i></button>
+    </div> */}
